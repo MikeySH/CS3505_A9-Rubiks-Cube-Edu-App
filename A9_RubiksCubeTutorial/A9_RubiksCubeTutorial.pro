536287b2a0bacd7cfde6581521607b12c8a69c67
Jimmy Trinh <jimmytrinh@Jimmys-MacBook-Pro.local>
2021-11-24 22:22:39 -0700

    added class example stuff

--- A9_RubiksCubeTutorial/A9_RubiksCubeTutorial.pro
+++ A9_RubiksCubeTutorial/A9_RubiksCubeTutorial.pro
@@ -10,15 +10,30 @@ CONFIG += c++11
 
 SOURCES += \
     main.cpp \
-    mainwindow.cpp
+    mainwindow.cpp \
+    scenewidget.cpp
 
 HEADERS += \
-    mainwindow.h
+    mainwindow.h \
+    scenewidget.h
 
 FORMS += \
     mainwindow.ui
+
+macx: LIBS += -L$$PWD/box2d/build/bin/ -lbox2d
+
+INCLUDEPATH += $$PWD/box2d/include
+DEPENDPATH += $$PWD/box2d/include
+
+macx: PRE_TARGETDEPS += $$PWD/box2d/build/bin/libbox2d.a
 
 # Default rules for deployment.
 qnx: target.path = /tmp/$${TARGET}/bin
 else: unix:!android: target.path = /opt/$${TARGET}/bin
 !isEmpty(target.path): INSTALLS += target
+
+
+DISTFILES += \
+    me.png
+
+RESOURCES +=
---
+++ A9_RubiksCubeTutorial/scenewidget.cpp
@@ -0,0 +1,76 @@
+#include "scenewidget.h"
+#include <QDebug>
+
+SceneWidget::SceneWidget(QWidget *parent) : QWidget(parent),
+    world(b2Vec2(0.0f, 10.0f)),
+    timer(this),
+    image("../../../me.png") // Make a resource file - mac executables are in a hidden folder
+{
+
+    // Define the ground body.
+    b2BodyDef groundBodyDef;
+    groundBodyDef.position.Set(0.0f, 20.0f);
+
+    // Call the body factory which allocates memory for the ground body
+    // from a pool and creates the ground box shape (also from a pool).
+    // The body is also added to the world.
+    b2Body* groundBody = world.CreateBody(&groundBodyDef);
+
+    // Define the ground box shape.
+    b2PolygonShape groundBox;
+
+    // The extents are the half-widths of the box.
+    groundBox.SetAsBox(50.0f, 10.0f);
+
+    // Add the ground fixture to the ground body.
+    groundBody->CreateFixture(&groundBox, 0.0f);
+
+    // Define the dynamic body. We set its position and call the body factory.
+    b2BodyDef bodyDef;
+    bodyDef.type = b2_dynamicBody;
+    bodyDef.position.Set(0.0f, 4.0f);
+
+    body = world.CreateBody(&bodyDef);
+
+    // Define another box shape for our dynamic body.
+    b2PolygonShape dynamicBox;
+    dynamicBox.SetAsBox(1.0f, 1.0f);
+
+    // Define the dynamic body fixture.
+    b2FixtureDef fixtureDef;
+    fixtureDef.shape = &dynamicBox;
+
+    // Set the box density to be non-zero, so it will be dynamic.
+    fixtureDef.density = 1.0f;
+
+    // Override the default friction.
+    fixtureDef.friction = 0.3f;
+    fixtureDef.restitution = 0.9;
+    // Add the shape to the body.
+    body->CreateFixture(&fixtureDef);
+    printf("Init world\n");
+
+    connect(&timer, &QTimer::timeout, this, &SceneWidget::updateWorld);
+    timer.start(50);
+}
+
+void SceneWidget::paintEvent(QPaintEvent *) {
+    // Create a painter
+    QPainter painter(this);
+    b2Vec2 position = body->GetPosition();
+    float angle = body->GetAngle();
+
+//    printf("%4.2f %4.2f %4.2f\n", position.x, position.y, angle);
+
+    painter.drawImage((int)(position.x*20), (int)(position.y*20), image);
+    painter.drawImage(200, 200, image);
+    qDebug() << image;
+    painter.end();
+   }
+
+void SceneWidget::updateWorld() {
+    // It is generally best to keep the time step and iterations fixed.
+    world.Step(1.0/60.0, 6, 2);
+
+    update();
+}
